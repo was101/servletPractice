a23d27469dac084f530ccf6914ee6c8fdcc1f223
whtjd <whtjd@DESKTOP-VVP1RKG>
2019-05-05 13:50:16 +0900

    커밋

--- WebContent/Welcome.jsp
+++ WebContent/Welcome.jsp
@@ -5,10 +5,25 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 </head>
+<script>
+$(document).ready(function() {
+$.ajax({
+	type:'post',
+	url:'dataIn',
+	success: function(result) {
+		$('.greed').text(result);
+	},
+	error: function() {
+		
+	}
+});
+});
+</script>
 <body>
 <div style="position: absolute;top: 20%;left: 50%;border:1px solid gray;padding: 50px;">
-<marquee style="background-color: lightskyblue;color: red;">${userID}님 환영합니다.</marquee>
+<marquee style="background-color: lightskyblue;color: red;"><%=request.getSession().getAttribute("userID") %>님 환영합니다.<br><span class="greed"></span></marquee>
 <form action="Logout" method="post"><br>
 <button type="submit">Logout</button>
 </form>
--- src/controller/dataIn.java
+++ src/controller/dataIn.java
@@ -1,26 +1,23 @@
 package controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class Login
+ * Servlet implementation class dataIn
  */
-@WebServlet("/Login")
-public class Login extends HttpServlet {
+@WebServlet("/dataIn")
+public class dataIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Login() {
+    public dataIn() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,22 +28,16 @@ public class Login extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		 response.setContentType("text/html;charset=UTF-8");
-		HttpSession session = request.getSession();
-		if(session.getAttribute("userID") == null) {
-			String userID = request.getParameter("userID");
-			session.setAttribute("userID", userID);
-			RequestDispatcher rd = request.getRequestDispatcher("Welcome.jsp");
-			rd.forward(request, response);
-		}else {
-			RequestDispatcher rd = request.getRequestDispatcher("Welcome.jsp");
-			rd.forward(request, response);
-		}
+		String str = "안녕하세요.";
+		System.out.println(str);
+		response.getWriter().write(str);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
